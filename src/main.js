65b4ed38e215ed0a098f3cd4561d6704463dad59
Witek Podskarbi <witdev.front@gmail.com>
2020-05-03 14:37:46 +0200

    feat: initial setup and style - opening modal form

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./style/main.scss";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+library.add(fas);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="centered">
+    <button class="btn btn-primary" @click="showModal = true">
+      Add new case
+    </button>
+    <BaseModal v-if="showModal" @close="showModal = false"></BaseModal>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import BaseModal from "@/components/BaseModal";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: { BaseModal },
+  data() {
+    return {
+      showModal: false
+    };
   }
 };
 </script>
+
+<style></style>
